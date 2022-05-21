bb083d41c8b2b168f6794f75994b55563d1900fe
Chase McCoy <chsmc@stripe.com>
2022-05-21 13:38:30 -0500

    meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,19 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta property='og:image' content='/social.png?v=1.0' />
+        <meta property='og:title' content='Redeclarations' />
+        <meta
+          name='description'
+          content='A reclaiming of the Declaration of Independence by those who never signed it.'
+        />
+        <meta
+          property='og:description'
+          content='A reclaiming of the Declaration of Independence by those who never signed it.'
+        />
+        <meta
+          property='og:image'
+          content='https://redeclarations.com/social.png?v=1.0'
+        />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
       </Head>
