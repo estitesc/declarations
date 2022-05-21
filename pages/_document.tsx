11732b3f1603d03cb3d0a76e0ec6e4c569b91b7c
Chase McCoy <chsmc@stripe.com>
2022-05-21 13:33:04 -0500

    add a social share image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta property='og:image' content='/social.png?v=1.0' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+      </Head>
       <body>
         <Main />
         <NextScript />
