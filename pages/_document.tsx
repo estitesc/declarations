f07f606aac8524e5f00e43adb8ca2f21d488a296
Chase McCoy <chsmc@stripe.com>
2022-05-22 18:51:51 -0500

    favicon and meta stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,12 +13,25 @@ export default function Document() {
           property='og:description'
           content='A reclaiming of the Declaration of Independence by those who never signed it.'
         />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           property='og:image'
           content='https://redeclarations.com/social.png?v=1.0'
         />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='redeclarations.com' />
+        <meta property='twitter:url' content='https://redeclarations.com/' />
+        <meta name='twitter:title' content='Redeclarations' />
+        <meta
+          name='twitter:description'
+          content='A reclaiming of the Declaration of Independence by those who never signed it.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://redeclarations.com/social.png?v=1.0'
+        />
       </Head>
       <body>
         <Main />
