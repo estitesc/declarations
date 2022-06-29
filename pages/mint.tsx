9b7f60181873984b2fbdd7ed26beb3ca70f3f050
Evan Stites-Clayton <stitesclayton@gmail.com>
2022-06-28 19:01:12 -0700

    Adds view on opensea

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Mint.module.css'
-import clsx from 'clsx'
-import { Logo } from '../c/Logo'
-import TextSelector from '../c/TextSelector'
-import { declarationText } from '../utils/declarationText'
-import useConnectWallet from '../h/useConnectWallet'
-import Declaration from '../c/Declaration'
-import useMint from '../h/useMint'
-import { useRouter } from 'next/router'
+import React from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Mint.module.css";
+import clsx from "clsx";
+import { Logo } from "../c/Logo";
+import TextSelector from "../c/TextSelector";
+import { declarationText } from "../utils/declarationText";
+import useConnectWallet from "../h/useConnectWallet";
+import Declaration from "../c/Declaration";
+import useMint from "../h/useMint";
+import { useRouter } from "next/router";
 
 type DeclarationPreviewProps = {
-  walletAddress?: string
-  selection?: any
-  loading?: boolean
-  onClick: () => void
-  connectWalletAndStoreAddress: () => void
-  setSelection: (arg0: any) => void
-  setTextSelKey: (arg0: number) => void
-  textSelKey: number
-  mobile?: boolean
-}
+  walletAddress?: string;
+  selection?: any;
+  loading?: boolean;
+  onClick: () => void;
+  connectWalletAndStoreAddress: () => void;
+  setSelection: (arg0: any) => void;
+  setTextSelKey: (arg0: number) => void;
+  textSelKey: number;
+  mobile?: boolean;
+};
 
 const DeclarationPreview = ({
   walletAddress,
@@ -37,19 +37,19 @@ const DeclarationPreview = ({
 }: DeclarationPreviewProps) => {
   return (
     <>
-      <Declaration size='500px' compact address={walletAddress}>
+      <Declaration size="500px" compact address={walletAddress}>
         {selection?.text}
       </Declaration>
 
-      <div className={styles.declaration} style={{ marginTop: '1rem' }}>
-        {walletAddress && (!selection || selection.text == '') && (
-          <div className={clsx(styles.subtitle, 'fluid-type')}>
+      <div className={styles.declaration} style={{ marginTop: "1rem" }}>
+        {walletAddress && (!selection || selection.text == "") && (
+          <div className={clsx(styles.subtitle, "fluid-type")}>
             Select words from the Declaration of Independence to create your
             redeclaration
           </div>
         )}
 
-        <div style={{ display: 'flex', gap: '1rem' }}>
+        <div style={{ display: "flex", gap: "1rem" }}>
           {!walletAddress && (
             <button
               className={clsx(styles.button)}
@@ -59,125 +59,136 @@ const DeclarationPreview = ({
             </button>
           )}
 
-          {walletAddress && selection && selection?.text !== '' && (
+          {walletAddress && selection && selection?.text !== "" && (
             <button
               className={clsx(styles.button, styles.mintButton)}
               onClick={onClick}
               disabled={loading}
             >
-              {loading ? 'Please wait...' : 'Mint your declaration'}
+              {loading ? "Please wait..." : "Mint your declaration"}
             </button>
           )}
 
-          {!(selection === null || selection?.text === '' || loading) && (
+          {!(selection === null || selection?.text === "" || loading) && (
             <button
               className={styles.button}
               onClick={() => {
-                setSelection({})
-                setTextSelKey(textSelKey + 1)
+                setSelection({});
+                setTextSelKey(textSelKey + 1);
               }}
-              disabled={selection === null || selection?.text === '' || loading}
+              disabled={selection === null || selection?.text === "" || loading}
             >
               Reset selection
             </button>
           )}
         </div>
 
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '.5rem' }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: ".5rem" }}>
           {walletAddress && (
             <div className={styles.walletAddress}>
               Connected: {walletAddress}
             </div>
           )}
           <a
-            href='https://twitter.com/RdclrtnsNFT'
+            href="https://twitter.com/RdclrtnsNFT"
             className={styles.link}
-            target='_blank'
-            rel='noreferrer'
+            target="_blank"
+            rel="noreferrer"
           >
             Follow the project on Twitter
           </a>
           <a
-            href='https://discord.gg/m5Q3bbYeGT'
+            href="https://discord.gg/m5Q3bbYeGT"
             className={styles.link}
-            target='_blank'
-            rel='noreferrer'
+            target="_blank"
+            rel="noreferrer"
           >
             Join us on Discord
+          </a>
+          <a
+            href="https://opensea.io/collection/rdclrtns"
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            View on OpenSea
           </a>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 const Mint: NextPage = () => {
-  const [selection, setSelection] = React.useState<any>({})
-  const [textSelKey, setTextSelKey] = React.useState(0)
+  const [selection, setSelection] = React.useState<any>({});
+  const [textSelKey, setTextSelKey] = React.useState(0);
   const [walletAddress, setWalletAddress] = React.useState<string | undefined>(
     undefined
-  )
-  const [loading, setLoading] = React.useState(false)
+  );
+  const [loading, setLoading] = React.useState(false);
 
-  const { query } = useRouter()
-  console.log('query is ', query, query.ownerId)
+  const { query } = useRouter();
+  console.log("query is ", query, query.ownerId);
 
-  const { mint } = useMint(query.ownerId as string | undefined)
+  const { mint } = useMint(query.ownerId as string | undefined);
 
   const onChange = React.useCallback((selection: any) => {
-    setSelection(selection)
-  }, [])
+    setSelection(selection);
+  }, []);
 
-  const connectWallet = useConnectWallet()
+  const connectWallet = useConnectWallet();
   const connectWalletAndStoreAddress = React.useCallback(async () => {
-    const address = await connectWallet()
-    setWalletAddress(address)
-  }, [connectWallet])
+    const address = await connectWallet();
+    setWalletAddress(address);
+  }, [connectWallet]);
 
   const generateAndMint = React.useCallback(async () => {
     if (loading) {
-      return
+      return;
     }
-    setLoading(true)
+    setLoading(true);
 
     const declarationBackground = document.getElementById(
-      'declaration-background'
-    )?.outerHTML
+      "declaration-background"
+    )?.outerHTML;
 
     const params = {
       text: selection.text.trim(),
       address: walletAddress,
-      width: '1600',
-      height: '1600',
+      width: "1600",
+      height: "1600",
       background: declarationBackground,
-    }
+    };
 
-    console.log('params are', params)
+    console.log("params are", params);
 
     //@ts-ignore
-    const paramString = new URLSearchParams(params).toString()
+    const paramString = new URLSearchParams(params).toString();
     const generateAndPinUrl = `https://screenshot-khaki.vercel.app/api?url=https://www.redeclarations.com/declaration?${new URLSearchParams(
       paramString
-    ).toString()}`
-    console.log('generate and pin URL is', generateAndPinUrl)
+    ).toString()}`;
+    console.log("generate and pin URL is", generateAndPinUrl);
 
     const response = await fetch(generateAndPinUrl, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    })
+    });
 
-    console.log('got response after generating and pinning the image', response)
+    console.log(
+      "got response after generating and pinning the image",
+      response
+    );
 
-    const data = await response.json()
-    const imageUrl = data.result
+    const data = await response.json();
+    const imageUrl = data.result;
 
-    console.log('now trying to mint with', imageUrl, selection.indices)
-    mint(imageUrl, selection.indices)
+    console.log("now trying to mint with", imageUrl, selection.indices);
+    mint(imageUrl, selection.indices);
 
-    setLoading(false)
-  }, [mint, selection.indices, selection.text, walletAddress, loading])
+    setLoading(false);
+  }, [mint, selection.indices, selection.text, walletAddress, loading]);
 
   return (
     <div className={clsx(styles.wrapper, loading && styles.wrapperLoading)}>
@@ -186,8 +197,8 @@ const Mint: NextPage = () => {
           <header className={styles.header}>
             <Logo className={styles.logo} />
             <p
-              className={clsx(styles.subtitle, 'fluid-type')}
-              style={{ marginTop: '1em' }}
+              className={clsx(styles.subtitle, "fluid-type")}
+              style={{ marginTop: "1em" }}
             >
               The Declaration of Independence marked America’s exit from the
               British Empire. By minting bits of the old declaration and
@@ -197,10 +208,10 @@ const Mint: NextPage = () => {
               us.
             </p>
             <div className={styles.instructions}>
-              <p style={{ marginTop: '1.5em' }}>
+              <p style={{ marginTop: "1.5em" }}>
                 Here’s how to create your own:
               </p>
-              <ol style={{ marginTop: '1em' }}>
+              <ol style={{ marginTop: "1em" }}>
                 <li>Click a word to begin a selection.</li>
                 <li>
                   Click on another word to select all of the text between the
@@ -243,7 +254,7 @@ const Mint: NextPage = () => {
 
       <div
         className={styles.desktopOnly}
-        style={{ position: 'sticky', top: '2rem' }}
+        style={{ position: "sticky", top: "2rem" }}
       >
         <DeclarationPreview
           walletAddress={walletAddress}
@@ -267,7 +278,7 @@ const Mint: NextPage = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Mint
+export default Mint;
